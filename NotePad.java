cb291876aabcc1b452b3d42c9b64bb196b1a550a
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-10 23:28:53 +0600

    1st submission

--- NotePad.java
+++ NotePad.java
@@ -16,7 +16,11 @@ public class NotePad {
         manuBar = new JMenuBar(); 
 
         // Step 1: Createing JFrame (Application Window)
-        frame.setBounds(0, 0, 400, 400); //Set Dimention of the frame
+        frame.setBounds(0, 0, 500, 500); //Set Dimention of the frame
+        frame.setTitle("NotePad"); // Set title of frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); ////When click button, exit out of application
+        frame.setLocationRelativeTo(null); // To open window in the middle
+
         frame.setLayout(null);
         frame.setJMenuBar(manuBar); // Add menubar into the frame
 
