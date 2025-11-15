628eda0a668c4dd506667060fa704d3d044370b1
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-15 22:13:07 +0600

    First submission

--- NotePad.java
+++ NotePad.java
@@ -2,55 +2,78 @@
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class NotePad {
+    // Declaring propertise of NotePad
+    JFrame frame;
 
-    // Creating Application Window
-    JFrame  frame;
-    JMenuBar manuBar;
+    JMenuBar menuBar;
+
     JMenu file, edit;
 
-    public NotePad() { // When ever this NotePad constructor call a frame will be created
+    JMenuItem newFile, openFile, saveFile;
+    JMenuItem cut, copy, paste, selectAll, close;
 
-        frame = new JFrame(); // Initialize a frame
-        manuBar = new JMenuBar(); 
+    JTextArea textArea;
 
-        // Step 1: Createing JFrame (Application Window)
-        frame.setBounds(0, 0, 500, 500); //Set Dimention of the frame
-        frame.setTitle("NotePad"); // Set title of frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); ////When click button, exit out of application
+    NotePad() {
+        frame = new JFrame(); //Initialize a frame
+        menuBar = new JMenuBar(); //Initialize a manubar
+        textArea = new JTextArea(); //Initialize a textarea
+
+
+
+        file = new JMenu("File"); //Initialize manu 1
+        edit = new JMenu("Edit"); //Initialize manu 2
+
+        //Initialize file manuItem
+        newFile = new JMenuItem("New Window"); 
+        openFile = new JMenuItem("Open File"); 
+        saveFile = new JMenuItem("Save File");
+
+        //Initialize edit menu items
+        cut = new JMenuItem("Cut");
+        copy = new JMenuItem("Copy");
+        paste = new JMenuItem("Paste");
+        selectAll = new JMenuItem("Select All");
+        close = new JMenuItem("Close");
+
+
+        //add menu item to edit manu
+        edit.add(cut);
+        edit.add(copy);
+        edit.add(paste);
+        edit.add(selectAll);
+        edit.add(close);
+
+        //add menu items to file manu
+        file.add(newFile); 
+        file.add(openFile);
+        file.add(saveFile);
+
+        //add menu1 to menubar
+        menuBar.add(file); 
+        menuBar.add(edit);
+        frame.setJMenuBar(menuBar); // set menuBar to frame
+        frame.add(new JScrollPane(textArea)); // add text area inside a scroll pane
+
+        frame.setBounds(0, 0, 500, 500);
+        frame.setTitle("NotePad");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null); // To open window in the middle
-
-        frame.setLayout(null);
-        frame.setJMenuBar(manuBar); // Add menubar into the frame
-
-
-
-
-        // Step 2: Creating manuBar
-
-        //Inicialize manubar
-        // Set menuBar to frame
-
+        // keep default BorderLayout so the text area fills the window
         
-        // Step 3: Create and complete the Manus(file, edit) before add to the manuBar and frame
-        file = new JMenu("File");
-        edit = new JMenu("Edit");
-        // Add manu to menuBar
-        manuBar.add(file);
-        manuBar.add(edit);
-
-
-
 
 
         frame.setVisible(true);
     }
+    
 
     
     public static void main(String[] args) {
         NotePad notePad = new NotePad();
     }
-
-
 }
